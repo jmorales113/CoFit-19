0bfd01711fc5f329928192a0b8d27f1181343959
Steven Boulom <sboulom@gmail.com>
2020-04-10 14:59:03 -0700

    edited micros handlebal and adding js file

---
+++ macroCal.js
@@ -0,0 +1,12 @@
+// male: 66 +  (6.3 x body weight in lbs) + (12.9 x height in) - (6.8 x age) = BMR
+// female: 655 + (4.3 x weight in lbs) + (4.7 x height in in) - (4.7 x age in years) = BMR
+//if decline to identify default: 66 +  (6.3 x body weight in lbs) + (12.9 x height in) - (6.8 x age) = BMR
+
+//BMR x 1.2 = noEx
+//BMR x 1.55 = moderateEx
+//BMR x 1.725 = regularEx
+
+
+//kg to lb = 1kg = 2.20462
+
+//if statement
