10916a285e6428de1bd878a2821781850d4164aa
elliotfouts <elliotfouts@gmail.com>
2020-04-14 15:09:56 -0700

    created and linked external stylesheets

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,16 +3,16 @@ const router = express.Router();
 
 router
 	.get('/', (req, res) => {
-		res.render('daily-log', { layout: 'myplan', title: 'Daily Log' });
+		res.render('daily-log', { layout: 'myplan', title: 'Daily Log' , cssLink: "../css/daily-log.css"});
 	})
 	.get('/bmi-calculator', (req, res) => {
-		res.render('bmi-calculator', { layout: 'myplan', title: 'BMI Calculator' });
+		res.render('bmi-calculator', { layout: 'myplan', title: 'BMI Calculator' , cssLink: "../css/bmi-calculator.css"});
 	})
 	.get('/macro-calculator', (req, res) => {
-		res.render('macro-calculator', { layout: 'myplan', title: 'Macro Calculator' });
+		res.render('macro-calculator', { layout: 'myplan', title: 'Macro Calculator' , cssLink: "../css/macro-calculator.css"});
 	})
 	.get('/workouts', (req, res) => {
-		res.render('workouts', { layout: 'myplan', title: 'Workouts' });
+		res.render('workouts', { layout: 'myplan', title: 'Workouts' , cssLink: "../css/workouts.css"});
 	});
 
 module.exports = router;
